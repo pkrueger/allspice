ffdc74f0d5ffc93dbc31d12ea8179c10e68ed280
pkrueger <sendthistopatrick@gmail.com>
2022-10-28 11:06:04 -0600

    Create Interface for repo items

--- allspice/Globals.cs
+++ allspice/Globals.cs
@@ -1,8 +1,6 @@
 global using System;
 global using System.Data;
 global using System.Collections.Generic;
-global using System.IO;
-global using System.Linq;
 
 global using Microsoft.AspNetCore.Authorization;
 global using Microsoft.AspNetCore.Mvc;
---
+++ allspice/Interfaces/IRepoItem.cs
@@ -0,0 +1,9 @@
+namespace allspice.Interfaces
+{
+  public interface IRepoItem<T>
+  {
+    T Id { get; set; }
+    DateTime CreatedAt { get; set; }
+    DateTime UpdatedAt { get; set; }
+  }
+}
