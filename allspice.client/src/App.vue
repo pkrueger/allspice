b902339eb1efcdaa35d4ea4f54cc0b7462e122ec
pkrueger <sendthistopatrick@gmail.com>
2022-10-31 16:43:53 -0600

    Top banner

--- allspice.client/src/App.vue
+++ allspice.client/src/App.vue
@@ -1,40 +1,35 @@
 <template>
-  <header>
-    <Navbar />
-  </header>
-  <main>
+  <main class="bg-light">
     <router-view />
   </main>
-   <footer class="bg-dark text-light">
-    Made with 💖 by CodeWorks
-  </footer>
 </template>
 
 <script>
-import { computed } from 'vue'
-import { AppState } from './AppState'
-import Navbar from './components/Navbar.vue'
+import { computed } from "vue";
+import { AppState } from "./AppState";
+import Navbar from "./components/Navbar.vue";
 
 export default {
   setup() {
     return {
-      appState: computed(() => AppState)
-    }
+      appState: computed(() => AppState),
+    };
   },
-  components: { Navbar }
-}
+  components: { Navbar },
+};
 </script>
 <style lang="scss">
 @import "./assets/scss/main.scss";
 
-:root{
+:root {
   --main-height: calc(100vh - 32px - 64px);
 }
 
+main {
+  padding: 1.5rem;
+}
 
-footer {
-  display: grid;
-  place-content: center;
-  height: 32px;
+.text-shadow {
+  text-shadow: 0.1rem 0.1rem 0.3rem rgba(0, 0, 0, 0.5);
 }
 </style>
--- allspice.client/src/pages/HomePage.vue
+++ allspice.client/src/pages/HomePage.vue
@@ -1,44 +1,166 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img
-        src="https://bcw.blob.core.windows.net/public/img/8600856373152463"
-        alt="CodeWorks Logo"
-        class="rounded-circle"
-      >
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 banner elevation-5">
+        <div class="top-banner">
+          <div class="search-bar">
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Search..."
+                aria-label="Search for recipe."
+                aria-describedby="search-button"
+              />
+            </div>
+          </div>
+          <div class="login">
+            <Login />
+          </div>
+        </div>
+        <div class="banner-text text-light text-shadow">
+          <h2 class="main">All-Spice</h2>
+          <h5 class="sub">Cherish Your Family And Their Cooking</h5>
+        </div>
+        <div class="filter-buttons bg-white shadow text-success">
+          <div class="filter-button-container">
+            <h5
+              :class="state.activeFilter == 'Home' ? 'active-filter' : ''"
+              class="filter-button"
+              @click="state.activeFilter = 'Home'"
+            >
+              Home
+            </h5>
+          </div>
+          <div class="filter-button-container">
+            <h5
+              :class="state.activeFilter == 'My Recipes' ? 'active-filter' : ''"
+              class="filter-button"
+              @click="state.activeFilter = 'My Recipes'"
+            >
+              My Recipes
+            </h5>
+          </div>
+          <div class="filter-button-container">
+            <h5
+              :class="state.activeFilter == 'Favorites' ? 'active-filter' : ''"
+              class="filter-button"
+              @click="state.activeFilter = 'Favorites'"
+            >
+              Favorites
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid content">
+    <div class="row">
+      <div class="col-12 recipes">
+        <RecipeCard />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { reactive } from "vue";
+import RecipeCard from "../components/RecipeCard.vue";
+import Login from "../components/Login.vue";
+
 export default {
   setup() {
-    return {}
-  }
-}
+    const state = reactive({
+      activeFilter: "Home",
+    });
+    return { state };
+  },
+  components: { RecipeCard, Login },
+};
 </script>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
+.banner {
+  background-image: url("banner.png");
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 21rem;
+  position: relative;
+  border-radius: 0.5rem;
 
-  .home-card {
-    width: 50vw;
+  .top-banner {
+    position: absolute;
+    right: 0;
+    top: 0;
+    display: flex;
+    align-items: end;
+    gap: 1rem;
+  }
 
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
+  .banner-text {
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    bottom: 50%;
+    transform: translateY(50%);
+    text-align: center;
+
+    .main {
+      font-weight: 700;
+      letter-spacing: 0.075rem;
+    }
+    .sub {
+      font-weight: 600;
+      letter-spacing: 0.04rem;
     }
   }
+
+  .filter-buttons {
+    --button-paddingX: 1.75rem;
+    --button-paddingY: 0.75rem;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: var(--button-paddingX);
+    position: absolute;
+    left: 50%;
+    bottom: -7.5%;
+    transform: translateX(-50%);
+    text-align: center;
+    padding: var(--button-paddingY) var(--button-paddingX);
+    border-radius: 0.25rem;
+
+    .filter-button {
+      width: 7rem;
+      margin-bottom: 0;
+      padding: 0.5rem 0;
+      cursor: pointer;
+    }
+
+    // .filter-button-container:hover .filter-button {
+    //   transform: translateY(-0.25rem);
+    //   box-shadow: 0 0.25rem 0.2rem -0.25rem rgba(0, 0, 0, 0.5);
+    // }
+
+    .active-filter {
+      border-bottom: 0.2rem solid #219653;
+      padding-bottom: 0.3rem;
+    }
+  }
+}
+
+.content {
+  .recipes {
+    display: grid;
+  }
+}
+
+.text-shadow {
+  text-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.766);
 }
 </style>
